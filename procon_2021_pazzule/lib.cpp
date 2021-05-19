d5329eabebc94e9b0aa365d2ef5e5fabf4f74f9a
riversunlight <aabbee107@icloud.com>
2021-05-19 23:47:13 +0900

    coloring debug show_table

--- procon_2021_pazzule/lib.cpp
+++ procon_2021_pazzule/lib.cpp
@@ -13,29 +13,35 @@ void show_table(vector<vector<int> > table) {
 		int sel_place = answer.sel_place[s];
 		cout << "‘I‘ðˆÊ’u: " << sel_place << endl;
 		for (int t = 0; t < answer.swap_cnt[s]; t++) {
+			int past_place = sel_place;
 			cout << "ˆÚ“®•ûŒü: " << answer.swap_operator[s][t] << endl;
 			switch (answer.swap_operator[s][t])
 			{
 			case 'U':
 				swap(table[sel_place / w][sel_place % w], table[(sel_place / w - 1 + h) % h][sel_place % w]);
-				sel_place -= w;
+				sel_place = ((sel_place / w - 1 + h) % h) * w + sel_place % w;
 				break;
 			case 'L':
 				swap(table[sel_place / w][sel_place % w], table[sel_place / w][(sel_place % w - 1 + w) % w]);
-				sel_place -= 1;
+				sel_place = sel_place / w * w + (sel_place % w - 1 + w) % w;
 				break;
 			case 'R':
 				swap(table[sel_place / w][sel_place % w], table[sel_place / w][(sel_place % w + 1) % w]);
-				sel_place += 1;
+				sel_place = sel_place / w * w + (sel_place % w + 1) % w;
 				break;
 			case 'D':
 				swap(table[sel_place / w][sel_place % w], table[(sel_place / w + 1) % h][sel_place % w]);
-				sel_place += w;
+				sel_place = ((sel_place / w + 1) % h) * w + sel_place % w;
 				break;
 			}
 			for (int y = 0; y < h; y++) {
 				for (int x = 0; x < w; x++) {
-					cout << table[y][x] << " ";
+					if (sel_place == x + y * w)
+						cout << "\033[31m" << table[y][x] << "\033[m" << " ";
+					else if (past_place == x + y * w)
+						cout << "\033[34m" << table[y][x] << "\033[m" << " ";
+					else
+					    cout << table[y][x] << " ";
 				}
 				cout << endl;
 			}
