c73542129d9393f304fddc512539f50dd50c7baa
riversunlight <aabbee107@icloud.com>
2021-08-21 15:22:53 +0900

    fix IDA*

--- procon_2021_pazzule/idastar.cpp
+++ procon_2021_pazzule/idastar.cpp
@@ -11,9 +11,9 @@ extern int h, w, sel_rate, swap_rate, sel_lim, search_dir;
 
 //IDA*‚É‚æ‚é‰æ‘œ•œŒ³’Tõ
 int max_depth = 0; //–³‘Ê‚ÈŒvŽZ‚ðÈ‚­‚½‚ß‚É+1‚Å‚Í‚È‚­‘O‰ñ‚ÌŒvŽZ‚Å“¾‚½Å‘P‚Ì•]‰¿’l‚É‚·‚é
-bool idastar(int depth, int count, Status status) {
-	if (status.place == complete) {
-		answer = status;
+bool idastar(int depth, int count, Status *status) {
+	if (status->place == complete) {
+		answer = *status;
 		return true;
 	}
 	if (depth == 0) return 0;
@@ -29,26 +29,24 @@ bool idastar(int depth, int count, Status status) {
 			for (int k = 0; k < ((search_dir)? 2 : 4); k++) {
 				//“ü‚ê‘Ö‚¦•û
 				int next_x = (x + d[0][k] + w) % w, next_y = (y + d[1][k] + h) % h;
-			    int pre_x = status.x, pre_y = status.y;
-				if (status.x == next_x  && status.y == next_y)
+			    int pre_x = status->x, pre_y = status->y;
+				if (status->x == next_x  && status->y == next_y)
 					continue;
-				if (status.x != x || status.y != y) {
-					if (status.sel_cnt >= sel_lim) continue;
-					status.sellect(x, y, sel_rate);
+				if (status->x != x || status->y != y) {
+					if (status->sel_cnt >= sel_lim) continue;
+					status->sellect(x, y, sel_rate);
 				}
 
 				//ŽÀÛ‚É“Ç‚Þ
-				status.move(d[0][k], d[1][k], h, w, swap_rate, goal_place);
-				if (depth >= status.eval_cost / 2 + ((status.eval_cost % 2 == 0) ? 0 : 1))
+				status->move(d[0][k], d[1][k], h, w, swap_rate, goal_place);
+				if (depth * 2 >= status->eval_cost)
 					clear = idastar(depth - 1, count + 1, status);
 				else
-					max_depth = min(max_depth, count + status.eval_cost / 2 + ((status.eval_cost % 2 == 0) ? 0 : 1));
-				if (clear) break;
-				status.unmove(pre_x, pre_y, h, w, sel_rate, swap_rate, goal_place);
+					max_depth = min(max_depth, count + status->eval_cost / 2 + ((status->eval_cost % 2 == 0) ? 0 : 1));
+				if (clear) return true;
+				status->unmove(pre_x, pre_y, h, w, sel_rate, swap_rate, goal_place);
 			}
-			if (clear) break;
 		}
-		if (clear) break;
 	}
 
 	return clear;
@@ -60,7 +58,7 @@ void idastar_solve(Status *status){
 	for (int depth = 1; !clear; depth = max_depth) {
 		max_depth = 10000000;
 		printf("[search]depth: %d\n", depth);
-		clear = idastar(depth, 0, *status);
+		clear = idastar(depth, 0, status);
 	}
 	answer.show();
 	answer.show_cost();
