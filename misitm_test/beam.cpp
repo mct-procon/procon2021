9442d2fc244ecf7e1dad1d120f52a638abebbba2
misitm <j1831@matsue-ct.jp>
2021-10-01 07:25:09 +0900

    バグ修正、出力部の追加

--- misitm_test/beam.cpp
+++ misitm_test/beam.cpp
@@ -7,7 +7,15 @@
 #include <queue>
 
 using namespace std;
-int PSIZE = 20;
+int PSIZE = 40;
+
+bool chmin(int& a, int b) {
+	if (a > b) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
 
 struct picture {
 	int div_x;
@@ -29,7 +37,7 @@ struct piece {
 
 picture inputpic() {
 	picture res;
-	FILE* fp = fopen("pictures/NGC_2336.ppm", "rb");
+	FILE* fp = fopen("problem.ppm", "rb");
 	if (fp == NULL) {
 		//読み込みエラー
 		assert(0);
@@ -47,6 +55,7 @@ picture inputpic() {
 	fscanf(fp, "%d %d\n", &res.size_x, &res.size_y);
 	fscanf(fp, "%d\n", &res.color_max);
 	res.pix = vector<vector<vector<unsigned char>>>(res.size_y, vector<vector<unsigned char>>(res.size_x, vector<unsigned char>(3)));
+
 	for (int i = 0; i < res.size_y; i++) {
 		for (int j = 0; j < res.size_x; j++) {
 			for (int k = 0; k < 3; k++) {
@@ -66,15 +75,15 @@ vector<piece> picture_2_pieces(const picture& pic) {
 	const int piece_num = pic.div_x * pic.div_y;
 	const int piece_size = pic.size_x / pic.div_x;
 	Image init_img(piece_size, piece_size, Palette::White);
-	vector<piece> res(piece_num, piece{ 0,vector<vector<vector<unsigned char>>>(0),DynamicTexture(init_img)});
+	vector<piece> res(piece_num, piece{ 0,vector<vector<vector<unsigned char>>>(0),DynamicTexture(init_img) });
 	for (int i = 0; i < piece_num; i++) {
 		res[i].piece_id = i;
 	}
 	int idx;
 	//上辺
 	idx = 0;
-	for (int i = 0; i < pic.size_y; i+= piece_size) {
-		for (int j = 0; j < pic.size_x; j+= piece_size) {
+	for (int i = 0; i < pic.size_y; i += piece_size) {
+		for (int j = 0; j < pic.size_x; j += piece_size) {
 			vector<vector<unsigned char>> tmp;
 			for (int k = 0; k < piece_size; k++) {
 				tmp.emplace_back(pic.pix[i][j + k]);
@@ -150,103 +159,229 @@ Image piece_2_image(piece p, int piece_size) {
 }
 */
 
-double calc_difference_score(vector<vector<unsigned char>> edge_a, vector<vector<unsigned char>> edge_b, int edge_width) {
-	double sum = 0;
+int calc_difference_score(vector<vector<unsigned char>> edge_a, vector<vector<unsigned char>> edge_b, int edge_width) {
+	int sum = 0;
 	for (int i = 0; i < edge_width; i++) {
 		for (int j = 0; j < 3; j++) {
-			sum += abs(edge_a[i][j] - edge_b[edge_width-1-i][j]);
+			//モザイクにしようとしたけど、最小誤差が良いのでは
+			//int mosaic = 0;
+			//int cnt = 0;
+			int mosa_width = 1;
+			int mindiv = INT_MAX;
+			for (int x = -mosa_width; x <= mosa_width; x++) {
+				if (i + x >= 0 && i + x < edge_width) {
+					//mosaic += edge_b[edge_width - 1 - (i + x)][j];
+					//cnt++;
+					chmin(mindiv, abs(edge_a[i][j] - edge_b[edge_width - 1 - (i + x)][j]));
+				}
+			}
+			//sum += abs(edge_a[i][j] - mosaic / cnt);
+			sum += mindiv;
 		}
 	}
+
 	return sum;
 }
 
-struct state { //ビームサーチで持つ状態
+struct state { //ビームサーチで持つ状態を改造した
 	vector<vector<int>> board; //その場所に何番のピースがあるか
 	vector<vector<int>> direction; //向き
-	vector<int> used; //使用済みピース (使用済みなら1)
-	int adjacent_num; //ピースが接する部分の数
-	double difference_score_sum; //ピースが接する部分の差異を数値化したものの総和
-	bool operator<(const state& another) const { //比較
-		if (adjacent_num == 0 || another.adjacent_num == 0)
-			return 0;
-		else
-			return -(difference_score_sum / pow(adjacent_num,1.5)) > -(another.difference_score_sum / pow(another.adjacent_num,1.5));
-	};
+	vector<int> pos;	//位置の逆引き(何番のピースかより)
 };
 
-state beam_search(const vector<piece>& pieces, const int& div_x, const int& div_y, const int& piece_size) {
-	const int beam_width = 5; //ビーム幅
+//差が小さいものを貪欲に選択していく
+state search(const vector<piece>& pieces, const int16& div_x, const int16& div_y, const int& piece_size) {
 	const int piece_num = div_x * div_y;
-	const int board_size_x = 4 * div_x;
-	const int board_size_y = 4 * div_y;
+	const int16 board_size_x = 520;
+	const int16 board_size_y = 520;
 	const int dy[4] = { -1,0,1,0 }, dx[4] = { 0,1,0,-1 };
-	
-	state init = { vector<vector<int>>(board_size_y,vector<int>(board_size_x,-1)),vector<vector<int>>(board_size_y,vector<int>(board_size_x,0)),vector<int>(piece_num,0),0,0 };
-	init.board[2*div_y][2*div_x] = 0;
-	init.used[0] = 1;
-	set<state> que;
-	que.insert(init);
-	for (int setup_piece_num = 1; setup_piece_num < piece_num; setup_piece_num++) {
-		for (int y = 1; y < board_size_y - 1; y++) {
-			for (int x = 1; x < board_size_x - 1; x++) {
-				if ((*que.begin()).board[y][x] == -1)
-					continue;
-				pieces[(*que.begin()).board[y][x]].tex.resized(PSIZE).rotated(90_deg*(*que.begin()).direction[y][x]).draw(x* PSIZE, y* PSIZE);
-			}
-		}
-		//Print << que.top().difference_score_sum;
-		System::Update();
-		set<state> next_que;
-		auto it = que.begin();
-		for (int i = 0; i < beam_width && it!=que.end(); i++) {
-			state now_state = *it; it++;
-			for (int y = 1; y < board_size_y-1; y++) {
-				for (int x = 1; x < board_size_x - 1; x++) {
-					if (now_state.board[y][x] != -1)
-						continue;
-					bool can_set = false;
-					for (int j = 0; j < 4; j++) {
-						if (now_state.board[y + dy[j]][x + dx[j]] != -1) {
-							can_set = true;
-							break;
-						}
-					}
-					if (can_set == false)
-						continue;
-					for (int pidx_a = 0; pidx_a < piece_num; pidx_a++) {
-						if (now_state.used[pidx_a])
-							continue;
-						now_state.board[y][x] = pidx_a;
-						now_state.used[pidx_a] = 1;
-						for (int dir_a = 0; dir_a < 4; dir_a++) {
-							int new_adjacent_num = 0;
-							double new_difference_score_sum = 0;
-							for (int j = 0; j < 4; j++) {
-								if (now_state.board[y + dy[j]][x + dx[j]] == -1)
-									continue;
-								int pidx_b = now_state.board[y + dy[j]][x + dx[j]];
-								int dir_b = now_state.direction[y + dy[j]][x + dx[j]];
-								new_adjacent_num++;
-								new_difference_score_sum += calc_difference_score(pieces[pidx_a].edge[(j - dir_a + 8) % 4], pieces[pidx_b].edge[(j - dir_b + 6) % 4], piece_size);
-							}
-							now_state.direction[y][x] = dir_a;
-							now_state.adjacent_num += new_adjacent_num;
-							now_state.difference_score_sum += new_difference_score_sum;
-							next_que.insert(now_state);
-							now_state.direction[y][x] = 0;
-							now_state.adjacent_num -= new_adjacent_num;
-							now_state.difference_score_sum -= new_difference_score_sum;
-						}
-						now_state.used[pidx_a] = 0;
-					}
-					now_state.board[y][x] = -1;
+
+	state ans = { vector<vector<int>>(board_size_y,vector<int>(board_size_x,-1)),vector<vector<int>>(board_size_y,vector<int>(board_size_x,0)),vector<int>(piece_num,-1) };
+	ans.board[256][256] = 0;
+	ans.pos[0] = ((256) << 16) + 256;
+
+	vector<bool> used(piece_num, 0);
+	used[0] = 1;
+
+	//diffをメモ
+	vector<vector<int>> diff_memo(piece_num * 4, vector<int>(piece_num * 4, -1));
+	//先にやる(並列)
+#pragma omp parallel for
+	for (int i = 0; i < piece_num; i++) {
+		int I = i * 4;
+		for (int id = 0; id < 4; id++) {
+			auto base_edge = pieces[i].edge[id];
+			for (int j = 0; j < piece_num; j++) {
+				int J = j * 4;
+				for (int jd = 0; jd < 4; jd++) {
+					diff_memo[I + id][J + jd] = calc_difference_score(base_edge, pieces[j].edge[jd], piece_size);
 				}
 			}
 		}
-		que = next_que;
 	}
-	return *que.begin();
+
+
+	set<int> next_pos;	//次に置く場所の候補
+	int next_pos_num = 4;
+	for (int i = 0; i < 4; i++) {
+		next_pos.insert(((256 + dy[i]) << 16) + 256 + dx[i]);
+	}
+
+	for (int count = 1; count < piece_num; count++) {	//全部のピース置くまで
+		int mi = INT_MAX;
+		int diff = 0;
+		int apos = 0;
+		int apiece = 0;
+		int aspin = 0;
+
+		for (auto nxpos : next_pos) {	//置く場所の候補より探索
+			for (int join_piece = 0; join_piece < piece_num; join_piece++) {
+				if (used[join_piece]) continue;	//まだ置いてないやつしか見ない
+				for (int join_spin = 0; join_spin < 4; join_spin++) {	//回転数
+					diff = 0;
+
+					int join_cnt = 0;
+					for (int join_d = 0; join_d < 4; join_d++) {	//接する辺を見る
+						int base_x = (nxpos & 65535) + dx[join_d];
+						int base_y = ((nxpos >> 16) & 65535) + dy[join_d];
+						assert(base_x < board_size_x);
+						assert(base_x >= 0);
+						assert(base_y < board_size_y);
+						assert(base_y >= 0);
+						int base_piece = ans.board[base_y][base_x];
+						if (base_piece == -1) continue;
+						join_cnt++;
+
+						//(n & 3) == (n % 4)
+						int b_direction = (join_d + 2 - ans.direction[base_y][base_x] + 4) & 3;
+						int j_direction = (join_d - join_spin + 4) & 3;
+						int bpd = base_piece * 4 + b_direction;	//base_piece_direction
+						int jpd = join_piece * 4 + j_direction;	//join_piece_direction
+						//if (diff_memo[bpd][jpd] == -1) {	//まだ差を見てないならメモする
+						//	diff_memo[bpd][jpd] = calc_difference_score(pieces[base_piece].edge[b_direction], pieces[join_piece].edge[j_direction], piece_size);
+						//}
+						assert(bpd < piece_num * 4);
+						assert(jpd < piece_num * 4);
+						diff += diff_memo[bpd][jpd];	//メモより計算
+					}
+
+					diff -= join_cnt * (INT_MAX / 100);	//多くの辺に接する形の方が良い
+					if (chmin(mi, diff)) {	//最も差が小さいものを選択
+						apos = nxpos;
+						apiece = join_piece;
+						aspin = join_spin;
+					}
+				}
+			}
+		}
+
+		//ピースを置く
+		int ans_x = apos & 65535;
+		int ans_y = (apos >> 16) & 65535;
+		ans.board[ans_y][ans_x] = apiece;
+		ans.direction[ans_y][ans_x] = aspin;
+		ans.pos[apiece] = apos;
+		used[apiece] = 1;
+
+		//描画
+		for (int x = 0; x < board_size_x; x++) {
+			for (int y = 0; y < board_size_y; y++) {
+				if (ans.board[y][x] == -1)
+					continue;
+				pieces[ans.board[y][x]].tex.resized(PSIZE).rotated(90_deg * ans.direction[y][x]).draw(x * PSIZE, y * PSIZE);
+			}
+		}
+		System::Update();
+
+		//接する辺が変わるので更新
+		for (int d = 0; d < 4; d++) {
+			int x = dx[d] + ans_x;
+			int y = dy[d] + ans_y;
+			if (ans.board[y][x] == -1) {
+				next_pos.insert((y << 16) + x);
+			}
+		}
+		next_pos.erase(apos);
+
+	}
+
+	/* そのまま置く
+	for (int i = 0; i < div_x; i++) {
+		for (int j = 0; j < div_y; j++) {
+			if (i == 0 && j == 0) continue;
+			ans.board[div_y + j][div_x + i] = j * div_x + i;
+		}
+	}
+	*/
+
+	return ans;
 }
+
+bool output(const state& ans, const int& board_size_x, const int& board_size_y, const int& div_x, const int& div_y) {
+	int ox = -1, oy = -1;
+	for (int y = 0; y < board_size_y; y++) {
+		for (int x = 0; x < board_size_x; x++) {
+			if (ans.board[y][x] != -1) {
+				ox = x, oy = y;
+				break;
+			}
+		}
+		if (ox != -1)
+			break;
+	}
+	for (int y = oy; y < oy + div_y; y++) {
+		for (int x = ox; x < ox + div_x; x++) {
+			if (ans.board[y][x] == -1) {
+				return 0;
+			}
+		}
+	}
+	vector<vector<int>> out_place(div_y, vector<int>(div_x));
+	vector<vector<int>> out_dir(div_y, vector<int>(div_x));
+	for (int y = oy; y < oy + div_y; y++) {
+		for (int x = ox; x < ox + div_x; x++) {
+			out_place[ans.board[y][x] / div_x][ans.board[y][x] % div_x] = (y - oy) * div_x + (x - ox);
+			out_dir[ans.board[y][x] / div_x][ans.board[y][x] % div_x] = ans.direction[y][x];
+		}
+	}
+	FILE* fp;
+	fp = fopen("output.txt", "w");
+	fprintf(fp, "%d %d\n", div_y, div_x);
+	for (int y = 0; y < div_y; y++) {
+		for (int x = 0; x < div_x; x++) {
+			if (x > 0) fprintf(fp, " ");
+			fprintf(fp, "%d", out_place[y][x]);
+		}
+		fprintf(fp, "\n");
+	}
+	for (int y = 0; y < div_y; y++) {
+		for (int x = 0; x < div_x; x++) {
+			if (x > 0) fprintf(fp, " ");
+			fprintf(fp, "%d", out_dir[y][x]);
+		}
+		fprintf(fp, "\n");
+	}
+	fclose(fp);
+	fp = fopen("output2.txt", "w");
+	fprintf(fp, "%d %d\n", div_y, div_x);
+	for (int y = 0; y < div_y; y++) {
+		for (int x = 0; x < div_x; x++) {
+			if (x > 0) fprintf(fp, " ");
+			fprintf(fp, "%d", ans.board[oy+y][ox+x]);
+		}
+		fprintf(fp, "\n");
+	}
+	for (int y = 0; y < div_y; y++) {
+		for (int x = 0; x < div_x; x++) {
+			if (x > 0) fprintf(fp, " ");
+			fprintf(fp, "%d", out_dir[y][x]);
+		}
+		fprintf(fp, "\n");
+	}
+	fclose(fp);
+	return 1;
+}
+
 
 void Main() {
 	Window::Resize(Size(1280, 720));
@@ -257,26 +392,34 @@ void Main() {
 		int r = pic.pix[i][j][0];
 		int g = pic.pix[i][j][1];
 		int b = pic.pix[i][j][2];
-		image[i][j] = Color(r,g,b);
+		image[i][j] = Color(r, g, b);
 	}
 	DynamicTexture texture(image);
 	texture.fill(image);
 	vector<piece> pieces = picture_2_pieces(pic);
-	state ans=beam_search(pieces, pic.div_x, pic.div_y, pic.size_x / pic.div_x);
-	const int board_size_x = 4 * pic.div_x;
-	const int board_size_y = 4 * pic.div_y;
-	int ax = 0, ay = 0;
+
+	state ans = search(pieces, pic.div_x, pic.div_y, pic.size_x / pic.div_x);
+
+	const int board_size_x = 520;
+	const int board_size_y = 520;
+	int ax = -256 * PSIZE, ay = -256 * PSIZE;
 	int selx = -1, sely = -1;
 	int wheel, shift;
-	while (1) {
+	while (System::Update()) {
 		if (KeyA.pressed())
-			ax+=2;
+			ax += 2;
 		if (KeyD.pressed())
-			ax-=2;
+			ax -= 2;
 		if (KeyW.pressed())
-			ay+=2;
+			ay += 2;
 		if (KeyS.pressed())
-			ay-=2;
+			ay -= 2;
+		if (KeySpace.down()) {
+			if (output(ans, board_size_x, board_size_y, pic.div_x, pic.div_y))
+				Print << U"Output succeeded";
+			else
+				Print << U"Output failed";
+		}
 		if (MouseL.up()) {
 			if (selx != -1) {
 				int putx = (Cursor::Pos().x - ax) / PSIZE;
@@ -309,7 +452,7 @@ void Main() {
 			PSIZE--;
 		}
 		else if (wheel == -1) {
-			if(PSIZE!=1)
+			if (PSIZE != 1)
 				PSIZE++;
 		}
 		shift = KeyLShift.pressed();
@@ -319,10 +462,10 @@ void Main() {
 					continue;
 				if (ans.board[y][x] == -1)
 					continue;
-					//Rect(x * 50 + ax, y * 50 + ay, x * 50 + ax + 50, y * 50 + ay + 50).draw(ColorF(1.0, 1.0, 1.0, 0.5));
+				//Rect(x * 50 + ax, y * 50 + ay, x * 50 + ax + 50, y * 50 + ay + 50).draw(ColorF(1.0, 1.0, 1.0, 0.5));
 				else {
 					pieces[ans.board[y][x]].tex.resized(PSIZE).rotated(90_deg * ans.direction[y][x]).draw(x * PSIZE + ax, y * PSIZE + ay);
-					if(shift)
+					if (shift)
 						font(ans.board[y][x]).draw(x * PSIZE + ax, y * PSIZE + ay, ColorF(0.5, 0.5, 0.5, 0.8));
 				}
 			}
@@ -334,6 +477,6 @@ void Main() {
 					font(ans.board[sely][selx]).draw(Cursor::Pos().x - PSIZE / 2, Cursor::Pos().y - PSIZE / 2, ColorF(0.5, 0.5, 0.5, 0.8));
 			}
 		}
-		System::Update();
 	}
+
 }
