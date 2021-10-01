3fe8e7b390ba1ffe7c405acd95860c5b172c804c
renju5599 <saming0811@gmail.com>
2021-10-01 17:25:11 +0900

    入力関係を修正しました

--- randomizer/randomizer/submittest/submit.rb
+++ randomizer/randomizer/submittest/submit.rb
@@ -1,4 +1,4 @@
-# 提出テスト
+﻿# 提出テスト
 # コマンドライン引数に"get"か"post"を指定
 # 提出時は./submit.txtの中身をpostする
 
@@ -6,7 +6,7 @@ require 'net/http'
 require 'uri'
 
 if ARGV[0] == 'get'
-  url = 'https://procon32-practice.kosen.work/problem.ppm'
+  url = 'https://procon32-pre.kosen.work/problem.ppm'
   res = Net::HTTP.get_response(URI.parse(url))
   puts "#{res.code} \"#{res.message}\""
   if res.is_a?(Net::HTTPSuccess)
@@ -16,7 +16,7 @@ if ARGV[0] == 'get'
   end
 elsif ARGV[0] == 'post'
   token = '60903ba069865cf2be2d22d1e58bb2a9e6fdc7275b0b5e9d6b0038118d28d2e9'
-  url = 'https://procon32-practice.kosen.work'
+  url = 'https://procon32-pre.kosen.work'
   data = ''
   File.open('submit.txt', 'r') do |file|
     res = Net::HTTP.post(URI.parse(url), file.read, { 'procon-token' => token })
