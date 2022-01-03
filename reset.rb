21bd67aee8cba2ed83c66e01e0fc145c605d6640
riversunlight <aabbee107@icloud.com>
2022-01-03 16:32:59 +0900

    いらないやつ消すreset.rb

---
+++ reset.rb
@@ -0,0 +1,8 @@
+# いらないファイルをまとめて削除
+
+system('del submit.txt')
+system('del puzzle_visualizer\submit.txt')
+system('del puzzle_visualizer\problem.ppm')
+system('del misitm_test\output2.txt')
+system('del misitm_test\problem.ppm')
+system('del puzzle\input.txt')
